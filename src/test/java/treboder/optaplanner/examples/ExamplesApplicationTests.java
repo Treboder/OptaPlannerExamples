7b79f3dd63e80c0df9619b6bc024fbe6208d2ba8
Treboder <robert.kohn@gmx.net>
2023-03-28 20:43:09 +0200

    Initial commit

---
+++ src/test/java/treboder/optaplanner/examples/ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package treboder.optaplanner.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
