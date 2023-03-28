e8907f017b8363d3b29c76dad9e0080bbb2141bf
Treboder <robert.kohn@gmx.net>
2023-03-28 20:52:28 +0200

    Add springdoc with hello-world controller

---
+++ src/main/java/treboder/optaplanner/examples/controller/HelloController.java
@@ -0,0 +1,25 @@
+package treboder.optaplanner.examples.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import treboder.optaplanner.examples.ExamplesApplication;
+
+@RestController
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExamplesApplication.class);
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        logger.info("Hello World");
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+
+
+
+
+}
